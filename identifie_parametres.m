70b0a51ba7d6a5a8f4132d6761880da17f65f4b0
rayanyahiaoui13 <rayan1.yahiaoui@gmail.com>
2026-09-11 09:42:43 +0800

    identifie parametres

---
+++ identifie_parametres.m
@@ -0,0 +1,19 @@
+A = [];
+b = [];
+
+for i = 1:length(mesures)
+    k = mesures(i);
+    
+    ligne1 = [alpha(k), alpha(k)-beta(k), 0, vit_alpha(k), vit_alpha(k)-vit_beta(k), 0, acc_alpha(k), 0, 0];
+    ligne2 = [0, beta(k)-alpha(k), beta(k)-gamma(k), 0, vit_beta(k)-vit_alpha(k), vit_beta(k)-vit_gamma(k), 0, acc_beta(k), 0];
+    ligne3 = [0, 0, gamma(k)-beta(k), 0, 0, vit_gamma(k)-vit_beta(k), 0, 0, acc_gamma(k)];
+
+    b1 = f(k);
+    b2 = 0;
+    b3 = 0;
+    
+    A = [A ; ligne1 ; ligne2 ; ligne3];
+    b = [b ; b1 ; b2 ; b3];
+end
+
+x_hat= A \ b;
